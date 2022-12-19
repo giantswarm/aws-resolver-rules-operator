b925f1301a80deb8fc6ca55e1349aedf3fdf074f
Berk Dehrioğlu <bdehrioglu@yahoo.com>
2022-12-19 12:13:44 +0300

    fix helm templates

--- main.go
+++ main.go
@@ -27,6 +27,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	capa "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
+	capi "sigs.k8s.io/cluster-api/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -42,7 +44,8 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
+	utilruntime.Must(capi.AddToScheme(scheme))
+	utilruntime.Must(capa.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
