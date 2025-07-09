7599b88ff86d408bda98755b1fa2005734ae840f
Jose Armesto <github@armesto.net>
2025-07-09 17:12:46 +0200

    Only use security groups and subnets if defined

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -119,7 +119,7 @@ coverage-html: test-unit
 CONTROLLER_GEN = $(shell pwd)/bin/controller-gen
 .PHONY: controller-gen
 controller-gen: ## Download controller-gen locally if necessary.
-	$(call go-get-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.5)
+	$(call go-get-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen@v0.18.0)
 
 ENVTEST = $(shell pwd)/bin/setup-envtest
 .PHONY: envtest
